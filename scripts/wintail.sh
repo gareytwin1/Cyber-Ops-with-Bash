959d834f4c25371e51511d7eb191571fa5ceacc2
gareytwin1 <gareytwin1@gmail.com>
2024-07-23 20:44:12 -0500

    Add ../files/ioc.txt and wintail.sh

---
+++ scripts/wintail.sh
@@ -0,0 +1,11 @@
+WINLOG="Application"
+LASTLOG=$(wevtutil qe "$WINLOG" //c:1 //rd:true //f:text)
+
+while true; do
+    CURRENTLOG=$(wevutil qe "$WINLOG" //c:1 //rd:true //f:text)
+    if [[ "$CURRENTLOG" != "$LASTLOG" ]]; then
+        echo "$CURRENTLOG"
+        echo "-------------------------------"
+        LASTLOG="$CURRENTLOG"
+    fi
+done
