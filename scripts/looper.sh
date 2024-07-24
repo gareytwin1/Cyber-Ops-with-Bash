ae8dab7c2759a5334255e68f8d7a6395ae36852e
gareytwin1 <gareytwin1@gmail.com>
2024-07-23 20:52:13 -0500

    Add looper.sh

---
+++ scripts/looper.sh
@@ -0,0 +1,13 @@
+interval(){
+    echo $(date '=%y%m%d %H%M%S') $cnt
+    cnt=0
+}
+
+declare -i cnt=0
+trap interval SIGUSR1
+
+shopt -s lastpipe
+
+tail -f --pid=$$ ${1:-log.file} | while read aline; do
+    let cnt++
+done
