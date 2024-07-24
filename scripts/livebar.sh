bc9da86d972c8dadf65e8decb8378b71897ef138
gareytwin1 <gareytwin1@gmail.com>
2024-07-24 10:26:34 -0500

    Add livebar.sh script

---
+++ scripts/livebar.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# livebar.sh
+#
+# Description:
+# Creates rolling horizontal bar chart of live data
+# 
+# Usage: 
+# <output from other script or program> | bash livebar.sh
+#
+
+function pr_bar(){
+    local raw maxraw scaled
+    raw=$1
+    maxraw=$2
+    ((scaled=(maxbar*raw)/maxraw))
+    ((scaled == 0) && scaled=1
+    for((i=0; i<scaled; i++)); do printf '#'; done
+    printf '\n'
+} # pr_bar
+
+maxbar=60 # largest no. of chars in a bar
+MAX=60
+while read dayst timst qty; do
+    if((qty > MAX)); then
+        let MAX=$qty/4  # allow some room
+        echo"       **** rescaling: MAX=$MAX"
+    fi
+    printf '%6.6s %6.6s %4d:' $dayst $timst $qty
+    pr_bar $qty $MAX
+done
+
+
