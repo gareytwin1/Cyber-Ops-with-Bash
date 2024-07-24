d3d46c1ed9faca0e0310b72be80932e4fbc18cd5
gareytwin1 <gareytwin1@gmail.com>
2024-07-24 12:26:49 -0500

    Add option -i number of seconds to tailcount.sh

--- scripts/tailcount.sh
+++ scripts/tailcount.sh
@@ -9,6 +9,11 @@
 #   filename: passed to looper.sh
 #
 
+function usage(){
+    echo "Usage: $0 [-i seconds] <input file>"
+    echo "  -i seconds: Integer representing the amount of seconds to sleep."
+}
+
 # cleanup - the other processes on exit
 function cleanup(){
     [[ -n $LOPID ]] && kill $LOPID
@@ -16,6 +21,20 @@ function cleanup(){
 
 # Start looper.sh in the background
 trap cleanup EXIT
+
+while getopts 'i:'; do
+    case $opts in
+        i)
+            numsecs=$OPTARG
+            if ! [[ "$numsecs" =~ ^[0-9]+$ ]] || [ "$numsecs" -lt 1 ]; then
+                echo "Error: -i option requires integer greater than 1"
+                usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
 
 bash looper.sh $1 &
 LOPID=$!
@@ -26,5 +45,5 @@ sleep 3
 # Send SIGUSR1 signal to looper.sh every 5 seconds
 while true; do
     kill -SIGUSR1 $LOPID
-    sleep 5
+    sleep $numsecs
 done >&2
