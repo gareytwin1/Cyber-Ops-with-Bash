7fbef0699a14b181b92151cf64ed187a68933352
gareytwin1 <gareytwin1@gmail.com>
2024-07-27 12:53:15 -0500

    Update vtjson.sh

--- scripts/vtjson.sh
+++ scripts/vtjson.sh
@@ -9,17 +9,18 @@
 #               default: Calc_VirusTotal.json
 #
 
-RE='^.(.*)...\{.*detect..(.,*),..vers.*result....(.*),..update.*$'
-
-FN="${1:-Calc_VirusTotal.json}"
-sed -e 's/{"scans": {/&\n /' -e 's/},/&\n/g' "$FN" |
-while read ALINE; do
-    if [[ $ALINE =~ $RE ]]; then
-        VIRUS=${BASH_REMATCH[1]}
-        FOUND=${BASH_REMATCH[2]}
-        RESLT=${BASH_REMATCH[3]}
-        if [[ $FOUND == .*true.* ]]; then
-            echo "Virus: " $VIRUS " - Result: " $RESLT
-        fi
-    fi
-done
+#RE='"detected":(true|false),"result":"([^"]*)","update":"([^"]*)","version":"([^"]*)"'
+#FN=${1:-Calc_VirusTotal.json}
+echo "File: " $1
+#sed -e 's/.*,"scans":/{"scans":\n/' -e 's/},/&\n/g' -e 's/}}.*/}}/' $FN |
+#while read -r ALINE; do
+#    echo "$ALINE"
+#    if [[ $ALINE =~ $RE ]]; then
+#        echo "Matched: $ALINE"
+#        DETECTED=${BASH_REMATCH[1]}
+#        RESULT=${BASH_REMATCH[2]}
+#        UPDATE=${BASH_REMATCH[3]}
+#        VERSION=${BASH_REMATCH[4]}
+#       echo "Detected: $DETECTED - Result: $RESULT - Update: $UPDATE - Version: $VERSION"
+#   fi
+#done
