1e3966454571ba8a0b5a98f1bfd069c1fec3524d
gareytwin1 <gareytwin1@gmail.com>
2024-07-29 13:12:38 -0500

    Update weblogfmt.sh

--- scripts/weblogfmt.sh
+++ scripts/weblogfmt.sh
@@ -10,7 +10,7 @@
 #
 
 function tagit(){
-    printf '<%s>%s</%s>/n' "${1}" "${2}" "${1}"
+    printf '<%s>%s</%s>\n' "${1}" "${2}" "${1}"
 }
 
 # basic header tags
@@ -23,20 +23,20 @@ echo "<th>IP Address</th>" # column header
 echo "<th>Date</th>"
 echo "<th>URL Requested</th>"
 echo "<th>Status Code</th>"
-echo "<th>Size<th>"
+echo "<th>Size</th>"
 echo "<th>Referrer</th>"
 echo "<th>User Agent</th>"
 echo "</tr>"
 
 while read f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12plus; do
     echo "<tr>"
-    tagit td "${f1}"
-    tagit td "${f4}" "${f5}"
-    tagit td "${f6}" "${f7}"
-    tagit td "${f9}"
-    tagit td "${f10}"
-    tagit td "${f11}"
-    tagit td "${f12plus}"
+    tagit "td" "${f1}"
+    tagit "td" "${f4} ${f5}"
+    tagit "td" "${f6} ${f7}" 
+    tagit "td" "${f9}"
+    tagit "td" "${f10}"
+    tagit "td" "${f11}"
+    tagit "td" "${f12plus}"
     echo "</tr>"
 done < $1
 
